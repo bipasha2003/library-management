a84e954cbd7b56c567525d8224d385e2e290563d
bipasha2003 <bagchibipasha745@gmail.com>
2024-10-05 23:17:55 +0530

    Fine Ammount Model Created

--- database/migrations/2024_10_05_174107_create_fine_ammounts_table.php
+++ database/migrations/2024_10_05_174107_create_fine_ammounts_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('book_has_prices', function (Blueprint $table) {
+        Schema::create('fine_ammounts', function (Blueprint $table) {
             $table->id();
-            $table->integer("book_id");
-            $table->integer("no_of_days_minimum");
+            $table->integer("no_of_days");
             $table->integer("rate_per_day");
+            
+            
         });
     }
 
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('book_has_prices');
+        Schema::dropIfExists('fine_ammounts');
     }
 };
